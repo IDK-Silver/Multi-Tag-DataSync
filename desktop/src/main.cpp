ce1dee12caebd0671bed5085cd708db5b31458ad
IDK-Silver <60951976+IDK-Silver@users.noreply.github.com>
2024-10-22 00:39:09 +0800

    add simply tray

--- desktop/src/main.cpp
+++ desktop/src/main.cpp
@@ -1,3 +1,20 @@
-int main() {
-    return 0;
+#include <QApplication>
+#include <QMessageBox>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    // 確保應用程式有系統托盤支持
+    if (!QSystemTrayIcon::isSystemTrayAvailable()) {
+        QMessageBox::critical(nullptr, "錯誤", "系統不支援系統托盤");
+        return -1;
+    }
+    QApplication::setQuitOnLastWindowClosed(false);
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
 }
