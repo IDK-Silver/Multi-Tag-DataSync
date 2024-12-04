be155062fb0e382189c884993a521e6acec9b20b
yu-feng <60951976+IDK-Silver@users.noreply.github.com>
2024-12-04 20:12:55 +0800

    and new API (#9)
    * remove idea file
    
    * change structure
    
    * add FileObject
    
    * change method name
    
    * add some feature
    
    get_file_info
    
    * finish add & update & delete file_info API
    
    * add root_uuid

---
+++ interface/app/api/v1/file/info.py
@@ -0,0 +1,122 @@
+from datetime import datetime
+
+from fastapi import APIRouter, Depends, HTTPException
+from app.core.security import get_current_user
+from app.schemas.auth import User
+from app.schemas.file.info import  DocUUIDSchema, DocInfoSchema, UpdateDocInfoSchema
+from app.modules.file_object.object import FileObject
+from app.core.db import DatabaseConnection
+
+router = APIRouter()
+
+@router.post("/", response_model=DocInfoSchema)
+async def get_file_info(doc_info: DocUUIDSchema, current_user: User = Depends(get_current_user)):
+    # check uuid is not empty
+    if len(doc_info.uuid) == 0:
+        raise HTTPException(
+            status_code=404,
+            detail="File uuid is required.",
+        )
+
+    file = FileObject.from_db(doc_info.uuid)
+
+    if file is None:
+        raise HTTPException(
+            status_code=404,
+            detail="File notfound.",
+        )
+
+    if not file.is_valid():
+        raise HTTPException(
+            status_code=404,
+            detail="File is not valid.",
+        )
+
+    info = DocInfoSchema(
+        filename=file.filename,
+        uuid=file.uuid.to_string(),
+        parent_id=file.parent_id.to_string(),
+        d_id=file.d_id,
+        hash=file.hash,
+        timestamp=file.timestamp
+    )
+
+    return info
+
+@router.post("/modify")
+async def update_file_info(doc_info: UpdateDocInfoSchema, current_user: User = Depends(get_current_user)):
+
+    db = DatabaseConnection.get_instance()
+
+    remote_doc = FileObject.from_db(doc_info.uuid)
+
+    if remote_doc is None:
+        query = """
+            INSERT INTO doc (filename, doc_uuid, parent_uuid, d_id, doc_hash, timestamp, uploaded_by)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """
+        params = (
+            doc_info.filename,
+            doc_info.uuid,
+            doc_info.parent_id,
+            doc_info.d_id,
+            doc_info.hash,
+            datetime.now(),
+            current_user.user_id
+        )
+        db.execute_update(query, params)
+        return HTTPException(
+            status_code=200,
+            detail="Inset new file info",
+        )
+    else:
+        db = DatabaseConnection.get_instance()
+        query = """
+            UPDATE doc
+            SET filename = %s, doc_uuid = %s, parent_uuid = %s, d_id = %s, doc_hash = %s, timestamp = %s, uploaded_by = %s
+            WHERE doc_uuid = %s
+            """
+        params = (
+            doc_info.filename,
+            doc_info.uuid,
+            doc_info.parent_id,
+            doc_info.d_id,
+            doc_info.hash,
+            datetime.now(),
+            current_user.user_id,
+            doc_info.uuid
+        )
+        db.execute_update(query, params)
+        return HTTPException(
+            status_code=200,
+            detail="Update file info",
+        )
+
+@router.delete("/modify")
+async def delete_file_info(doc_info: DocUUIDSchema, current_user: User = Depends(get_current_user)):
+    db = DatabaseConnection.get_instance()
+    remote_doc = FileObject.from_db(doc_info.uuid)
+
+    if remote_doc is None:
+        return HTTPException(
+            status_code=404,
+            detail="File notfound.",
+        )
+
+    if remote_doc.is_root():
+        return HTTPException(
+            status_code=404,
+            detail="Can't delete root file.",
+        )
+
+    db.execute_update(
+        "DELETE FROM doc WHERE doc_uuid = %s",
+        (doc_info.uuid, )
+    )
+
+    return HTTPException(
+        status_code=200,
+        detail="File deleted Successfully.",
+    )
+
+
--- interface/app/api/v1/router.py
+++ interface/app/api/v1/router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
-# from app.api.v1.endpoints import auth, data, queue
-from app.api.v1.endpoints import auth
+# from app.api.v1.auth import auth, data, queue
+from app.api.v1.auth import router as auth_router
+from app.api.v1.file import router as file_router
 
 
 api_router = APIRouter()
 
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-# api_router.include_router(data.router, prefix="/data", tags=["data"])
-# api_router.include_router(queue.router, prefix="/queue", tags=["queue"])
+api_router.include_router(auth_router.router, prefix="/auth", tags=["auth"])
+api_router.include_router(file_router.router, prefix="/file", tags=["file"])
